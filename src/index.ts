c6790f0284f068c3e887851dde137c942fb1fafe
Tinnaphop Choonchuachan <mistertfy64@mistertfy64.com>
2023-11-27 21:38:47 +0700

    change: unfinished first commit, will prob abandon, will work on when bored

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import path from "path";
+
+const app = express();
+const PORT = 10003;
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "server/views"));
+
+app.get("/", (request: Request, response: Response) => {
+	response.render("pages/index.ejs");
+});
+
+app.listen(PORT, () => {
+	console.log(`App listening on port ${PORT}`);
+});
